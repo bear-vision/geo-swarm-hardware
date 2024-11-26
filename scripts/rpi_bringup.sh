11d6bf7a94b27671ac6ab39f80295fd00b198943
corygeodrone <corygeodrone@gmail.com>
2024-11-25 19:13:43 -0800

    rpi bringup script update

--- scripts/rpi_bringup.sh
+++ scripts/rpi_bringup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Source the local setup script
+cd ~/geo-swarm-hardware/ros2-ws
+source install/local_setup.bash
+
+# Launch nodes - TODO write launch file
+
 # Call MicroXRCEAgent to set up the serial connection with Pixhawk
 MicroXRCEAgent serial --dev /dev/ttyAMA0 -b 115200
-
-# Call launch files
