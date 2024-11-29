312bb501ab8aebdaf137d58cdcad4fa1b767be91
Calix <calix.tang@gmail.com>
2024-11-29 11:08:46 -0800

    Remove drone testing code from rpi master node, add sprayer on and off service in rpi master node

--- ros2-ws/src/rpi_master/rpi_master/rpi_master_node.py
+++ ros2-ws/src/rpi_master/rpi_master/rpi_master_node.py
@@ -4,7 +4,8 @@ from std_srvs.srv import Trigger
 from std_msgs.msg import Empty
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleLocalPosition, VehicleStatus
-#TODO: import some custom message or service type for servo control
+
+# Lots of code in here is re-used from https://github.com/PX4/px4_ros_com/blob/main/src/examples/offboard_py/offboard_control.py        
 
 
 class RPiMasterNode(Node):
@@ -21,45 +22,48 @@ class RPiMasterNode(Node):
             depth=1
         )
 
-        #TODO: Set up Service Proxy for servo node
+        # Set up service proxies for servo node
         self.sprayer_on_client = self.create_client(Trigger, 'sprayer_on')
         self.sprayer_off_client = self.create_client(Trigger, 'sprayer_off')
+        if not self.sprayer_on_client.wait_for_service(timeout_sec = 2.0):
+            self.get_logger().warn("Sprayer on client not set up.")
+        else:
+            self.get_logger().info("Sprayer on client set up.")
+        if not self.sprayer_off_client.wait_for_service(timeout_sec = 2.0):
+            self.get_logger().warn("Sprayer off client not set up.")
+        else:
+            self.get_logger().info("Sprayer off client set up.")
+
+        # Set up sprayer services that rpi can control
+        self.sprayer_off_srv = self.create_service(Trigger, '/rpi_master/rpi_sprayer_on', self.rpi_sprayer_on_callback)
+        self.sprayer_on_srv = self.create_service(Trigger, '/rpi_master/rpi_sprayer_off', self.rpi_sprayer_off_callback)
+
+        # Set up waypoint navigation service
         
-
-        #TODO: Set up Subscribers (or service!) for RPi to listen to laptop
-        # self.create_subscription(Empty, 'temporary_rpi_topic', self.laptop_command_callback, 10)
-
-        # Set up Subscribers for Pixhawk
+        # Set up subscribers for Pixhawk
         self.vehicle_local_position_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
         self.vehicle_status_subscriber = self.create_subscription(VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile)
 
-        #TODO: Set up Publishers for Pixhawk
-        # following the example in https://github.com/PX4/px4_ros_com/blob/main/src/examples/offboard/offboard_control.cpp for now
+        # Set up publishers for Pixhawk
         self.offboard_control_mode_publisher = self.create_publisher(OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
         self.trajectory_setpoint_publisher = self.create_publisher(TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
         self.vehicle_command_publisher = self.create_publisher(VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
 
-
-        # Create a timer to push offboard command mode messages if self.pi_command_mode is true every 0.5 seconds
-        self.timer = self.create_timer(0.1, self.offboard_command_timer_callback)
-
-        # Other class fields
+        # Class fields - some of these may not be used later
         self.offboard_setpoint_counter = 0
         self.vehicle_local_position = VehicleLocalPosition()
         self.vehicle_status = VehicleStatus()
+        self.max_height = -1.0 #in PX4 coords for now, can change later
 
-        # temporary fields for hardware testing
-        self.reached_hover_pt = False
-        self.hover_pt_reach_start_time = -1
-        self.takeoff_height = -1.0 #1 meter above where the drone starts
-
+        # TODO - push offboard command signals when ready
+        # self.timer = self.create_timer(0.1, self.offboard_command_timer_callback)
 
     def vehicle_local_position_callback(self, vehicle_local_position):
-        """Callback function for vehicle_local_position topic subscriber."""
+        """Update vehicle local position."""
         self.vehicle_local_position = vehicle_local_position
 
     def vehicle_status_callback(self, vehicle_status):
-        """Callback function for vehicle_status topic subscriber."""
+        """Update vehicle status."""
         self.vehicle_status = vehicle_status
 
     def publish_vehicle_command(self, command_id, *args):
@@ -86,10 +90,7 @@ class RPiMasterNode(Node):
 
     def arm(self):
         '''
-            Arms the drone.
-
-            Python implementation of C++ Example at https://github.com/PX4/px4_ros_com/blob/main/src/examples/offboard/offboard_control.cpp
-
+            Sends an arm command to the drone. Note that arming only happens when all pre-flight checks pass, so this isn't guaranteed to work.
         '''
 
         # https://github.com/PX4/px4_msgs/blob/ffb6e80e1c17e5714395611a020c282a87af8fa4/msg/VehicleCommand.msg#L78C1-L78C99
@@ -100,43 +101,74 @@ class RPiMasterNode(Node):
     def disarm(self):
         '''
             Disarms the drone.
-
-            Python implementation of C++ Example at https://github.com/PX4/px4_ros_com/blob/main/src/examples/offboard/offboard_control.cpp
         '''
 
         # https://github.com/PX4/px4_msgs/blob/ffb6e80e1c17e5714395611a020c282a87af8fa4/msg/VehicleCommand.msg#L78C1-L78C99
         self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0)
-        self.get_logger().info("Arm command sent.")
+        self.get_logger().info("Disarm command sent.")
 
-    def sprayer_on(self):
-        while not self.sprayer_on_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Sprayer on service not available, waiting again...')
+    def rpi_sprayer_on_callback(self, request, response):
+        def handle_auxiliary_response(future, response):
+            try:
+                auxiliary_response = future.result()
+                if auxiliary_response.success:  # Assuming the auxiliary service has a boolean `success` field
+                    # self.get_logger().info("Sprayer on call success.")
+                    response.message = "Success"
+                    response.success = True
+                else:
+                    self.get_logger().warn("Sprayer on call failed.")
+                    response.message = "Sprayer on call failed."
+                    response.success = False 
+            except Exception as e:
+                response.message = "Could not call sprayer_on service"
+                self.get_logger().error(f"Failed to call servo sprayer on service: {e}")
+                response.success = False
+        
+        if not self.sprayer_on_client.service_is_ready():
+            if not self.sprayer_on_client.wait_for_service(timeout_sec=2.0):
+                response.success = False
+                response.message = "Servo node sprayer_on service timed out."
+        
         request = Trigger.Request()
-
         future = self.sprayer_on_client.call_async(request)
 
-        # add a callback upon completion - not sure if necessary
-        # future.add_done_callback(self.callback)
+        future.add_done_callback(lambda f: handle_auxiliary_response(f, response))
+        return response
 
-    def sprayer_off(self):
-        while not self.sprayer_off_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Sprayer off service not available, waiting again...')
+    def rpi_sprayer_off_callback(self, request, response):
+        def handle_auxiliary_response(future, response):
+            try:
+                auxiliary_response = future.result()
+                if auxiliary_response.success:
+                    # self.get_logger().info("Sprayer off call success.")
+                    response.message = "Success"
+                    response.success = True  # Set the response based on auxiliary service success
+                else:
+                    self.get_logger().warn("Sprayer off call failed.")
+                    response.message = "Sprayer off call failed."
+                    response.success = False 
+            except Exception as e:
+                response.message = "Could not call sprayer_off service"
+                self.get_logger().error(f"Failed to call servo sprayer off service: {e}")
+                response.success = False
+        
+        if not self.sprayer_off_client.service_is_ready():
+            if not self.sprayer_off_client.wait_for_service(timeout_sec=2.0):
+                response.success = False
+                response.message = "Servo node sprayer_on service timed out."
+        
         request = Trigger.Request()
+        future = self.sprayer_on_client.call_async(request)
 
-        future = self.sprayer_off_client.call_async(request)
+        future.add_done_callback(lambda f: handle_auxiliary_response(f, response))
+        return response
 
-        # add a callback upon completion - not sure if necessary
-        # future.add_done_callback(self.callback)
+
 
     def land(self):
         """Switch to land mode."""
         self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
         self.get_logger().info("Switching to land mode")
-
-    # TODO: fill out when we have a better idea of how to implement 
-    def laptop_command_callback(self, message):
-        pass
-
 
     # Publishes an offboard ctrl mode message to the pixhawk.
     def publish_offboard_control_mode(self):
@@ -156,65 +188,6 @@ class RPiMasterNode(Node):
 
         msg.timestamp = self.get_clock().now().nanoseconds // 1000
         self.offboard_control_mode_publisher.publish(msg)
-
-    def publish_hover_message(self):
-        '''
-            Publishes a trajectory setpoint to the pixhawk, telling it to hover at 1 meter above the ground if possible.
-        
-            This is the Python version of C++ example code in
-                https://docs.px4.io/main/en/ros2/offboard_control.html
-        '''        
-
-        msg = TrajectorySetpoint()
-
-        #makes the drone hover at 1 meter in world coordinates (note: PX4 coordinate frame)
-        msg.position = [0.0, 0.0, -1.0]
-        msg.yaw = -3.14
-
-        msg.timestamp = self.get_clock().now().nanoseconds // 1000
-
-        self.trajectory_setpoint_publisher.publish(msg)
-
-
-    def offboard_command_timer_callback(self):
-        # if self.offboard_control:
-        #     self.publish_offboard_control_mode()
-        #     self.get_logger().debug('RPi Master published offboard command mode to Pixhawk.')
-        
-        if self.offboard_setpoint_counter == 10:
-            self.get_logger().info("RPi Master timer callback 10th call!")
-
-            #send the command to switch to offboard mode
-            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 1.0, 6.0)
-
-            #arm
-            self.arm()
-        
-
-        self.publish_offboard_control_mode()
-
-        #either try to get to hover pt, or track how long we have 
-        if not self.reached_hover_pt:
-            self.publish_hover_message()
-
-            if self.vehicle_local_position.z <= -1.0:
-                self.reached_hover_pt = True 
-
-                #sets the start time in milliseconds
-                self.hover_pt_reach_start_time = self.get_clock().now().nanoseconds // 1_000_000
-        else:
-            if (self.get_clock().now().nanoseconds // 1_000_000) - self.hover_pt_reach_start_time > 3000:
-                # we have been hovering for 3 seconds - time to land
-                self.land()
-                exit(0) # TODO: verify if this is the behavior we want in the test            
-
-       
-        if self.offboard_setpoint_counter < 11:
-            self.offboard_setpoint_counter += 1
-
-    
-
-
 
 def main(args = None):
     # Initialize node
