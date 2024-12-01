5d773406d2893f3abda9c512afd86a39a4bc23cc
Ale-ZJ <alexandrazj28@gmail.com>
2024-12-01 14:21:42 -0800

    create launch for pub, action, and path planning node to test laptop master node

---
+++ ros2-ws/src/laptop_master/launch/test_laptop_master.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='path_planning',
+            executable='path_planning_node',
+            name='path_planning_node'
+        ),
+        Node(
+            package='laptop_master',
+            executable='dummy_pub_node',
+            name='dummy_pub_node'
+        ),
+        Node(
+            package='laptop_master',
+            executable='dummy_action_server_node',
+            name='dummy_action_server_node'
+        )
+    ])
