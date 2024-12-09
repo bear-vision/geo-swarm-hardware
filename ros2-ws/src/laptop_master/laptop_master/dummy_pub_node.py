218a84b20041ba587d7061bf0cb14e3218e114a3
Calix <calix.tang@gmail.com>
2024-12-08 16:48:39 -0800

    fix bad syntax

--- ros2-ws/src/laptop_master/laptop_master/dummy_pub_node.py
+++ ros2-ws/src/laptop_master/laptop_master/dummy_pub_node.py
@@ -5,29 +5,36 @@ For testing
 import rclpy
 from rclpy.node import Node
 from time import sleep
-from geometry_msgs.msg import Position, Quaternion, Pose
+from geometry_msgs.msg import Point, Quaternion, Pose
 from px4_msgs.msg import VehicleLocalPosition, VehicleAttitude
 from custom_interfaces.msg import PerceptionStuff
 from rpi_master import rpi_master_utils
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 
 
 class DummyPubNode(Node):
     def __init__(self):
         super().__init__('position_pub_node')
         self.get_logger().debug("Initialize dummy pub node.")
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
 
         #paint locations in ROS2 frame as defined in the PX4 autopilot tower model file
-        self.paint_locations = [Position(x = 0.475, y = 0.0, z = 1.0), Position(x = -0.475, y = 0.0, z = 2.0), Position(x = 0.0, y = 0.475, z = 6.0), Position(x = 0.0, y = -0.475, z = 9.0)]
+        self.paint_locations = [Point(x = 0.475, y = 0.0, z = 1.0), Point(x = -0.475, y = 0.0, z = 2.0), Point(x = 0.0, y = 0.475, z = 6.0), Point(x = 0.0, y = -0.475, z = 9.0)]
         
         # stores local position in PX4 frame
         self.drone_posn = VehicleLocalPosition()
-        self.drone_orientatino = VehicleAttitude()
+        self.drone_orientation = VehicleAttitude()
 
         self.drone_pose = Pose()
 
         # continuously update drone local position (PX4 frame)
-        self.vehicle_attitude_subscriber = self.create_subscription(VehicleAttitude, '/fmu/out/vehicle_attitude', self.vehicle_attitude_callback, qos_profile)
-        self.vehicle_local_position_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, 10)
+        self.drone_orientation_subscriber = self.create_subscription(VehicleAttitude, '/fmu/out/vehicle_attitude', self.vehicle_attitude_callback, qos_profile)
+        self.drone_posn_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
         
         self.perception_pub = self.create_publisher(PerceptionStuff, 'realsense/out/perception_stuff', 10)
         
@@ -56,12 +63,12 @@ class DummyPubNode(Node):
     
     def vehicle_local_position_callback(self, vehicle_local_position):
         """Update vehicle local position."""
-        self.vehicle_local_position = vehicle_local_position
+        self.drone_posn = vehicle_local_position
         self.update_vehicle_local_pose()
 
     def vehicle_attitude_callback(self, vehicle_attitude):
         """Update vehicle attitude (orientation)."""
-        self.vehicle_attitude = vehicle_attitude
+        self.drone_orientation = vehicle_attitude
 
     def update_vehicle_local_pose(self):
         """
@@ -69,15 +76,15 @@ class DummyPubNode(Node):
         """
         #we assume that the current local position and vehicle attitude are time-synced.
 
-        if self.vehicle_local_position and self.vehicle_attitude:
+        if self.drone_posn and self.drone_orientation:
             px4_drone_pose = Pose()
-            px4_drone_pose.position.x = self.vehicle_local_position.x
-            px4_drone_pose.position.y = self.vehicle_local_position.y
-            px4_drone_pose.position.z = self.vehicle_local_position.z
-            px4_drone_pose.orientation.w = float(self.vehicle_attitude.q[0])
-            px4_drone_pose.orientation.x = float(self.vehicle_attitude.q[1])
-            px4_drone_pose.orientation.y = float(self.vehicle_attitude.q[2])
-            px4_drone_pose.orientation.z = float(self.vehicle_attitude.q[3])
+            px4_drone_pose.position.x = self.drone_posn.x
+            px4_drone_pose.position.y = self.drone_posn.y
+            px4_drone_pose.position.z = self.drone_posn.z
+            px4_drone_pose.orientation.w = float(self.drone_orientation.q[0])
+            px4_drone_pose.orientation.x = float(self.drone_orientation.q[1])
+            px4_drone_pose.orientation.y = float(self.drone_orientation.q[2])
+            px4_drone_pose.orientation.z = float(self.drone_orientation.q[3])
 
             ros2_drone_pose = rpi_master_utils.px4_to_ros_world_frame_transform(px4_drone_pose)
             return ros2_drone_pose
